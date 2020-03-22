edfd4526824e107b2df4b2812e2d0fb2a1586d4c
kuti gbolahan <kutigbolahan@ymail.com>
2020-03-22 13:35:00 +0100

    Changes to be committed:

--- lib/views/splashscreen.dart
+++ lib/views/splashscreen.dart
@@ -5,39 +5,61 @@ import 'package:liquid_swipe/liquid_swipe.dart';
 class Splash extends StatelessWidget {
   final pages = [
     Container(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Container(
-              width: double.infinity,
-              alignment: Alignment.center,
-              child: Image.asset(
-                'assets/images/3.jpg',
-                fit: BoxFit.contain,
-              )),
-          SizedBox(height: 30),
-          Container(
-            child: Text(
-              'Top News',
-              style: GoogleFonts.blinker (
-                fontSize: 35
-              )
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(top: 50,left: 10),
+                child: Text('Top News', style: GoogleFonts.blinker(fontSize: 30,
+                color: Colors.red
+                )),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 50, right: 10),
+                child: Text('Skip', style: GoogleFonts.blinker(fontSize: 25,
+                
+                )),
+              ),
+            ]),
+            SizedBox(height: 120),
+            Container(
+                width: double.infinity,
+                alignment: Alignment.center,
+                child: Image.asset(
+                  'assets/images/6.png',
+                  fit: BoxFit.contain,
+                )),
+            SizedBox(height: 30),
+             Container(
+              child: Text(
+                  'BE \n UPDATED.',
+                  style: GoogleFonts.blinker(
+                      fontSize: 35,
+                      fontWeight: FontWeight.w500,
+                      wordSpacing: 1.5,
+                      color: Colors.grey
+                      )),
             ),
-            
-          ),
-          SizedBox(height: 30),
-          Container(
-            child: Text(
-              'Be updated about the latest \n happening around the world.',
-              style: GoogleFonts.blinker (
-                fontSize: 25,
-                fontWeight: FontWeight.w200,
-                wordSpacing: 1.5
-              )
-            ),
-            
-          )
-        ],
+            SizedBox(height: 20),
+            Container(
+              child: Text(
+                  'Be updated about the latest \n happening around the world.',
+                  style: GoogleFonts.blinker(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w500,
+                      wordSpacing: 1.5,
+                      color: Colors.black
+                      )),
+            )
+          ],
+        ),
       ),
     ),
     Container(
@@ -52,37 +74,27 @@ class Splash extends StatelessWidget {
                 'assets/images/1.jpg',
                 fit: BoxFit.contain,
               )),
-          
-         
           SizedBox(height: 30),
           Container(
-            child: Text(
-              'Top News',
-              style: GoogleFonts.blinker (
-                fontSize: 35,
-                color: Colors.white
-              )
-            ),
-            
+            child: Text('Top News',
+                style: GoogleFonts.blinker(fontSize: 35, color: Colors.white)),
           ),
           SizedBox(height: 30),
           Container(
             child: Text(
-              'Read the latest news\n anywhere,anyplace\n and at anytime.',
-              style: GoogleFonts.blinker (
-                fontSize: 25,
-                fontWeight: FontWeight.w200,
-                wordSpacing: 1.5,
-                color: Colors.white,
-              )
-            ),
-            
+                'Read the latest news\n anywhere,anyplace\n and at anytime.',
+                style: GoogleFonts.blinker(
+                  fontSize: 25,
+                  fontWeight: FontWeight.w200,
+                  wordSpacing: 1.5,
+                  color: Colors.white,
+                )),
           )
         ],
       ),
     ),
     Container(
-      color: Colors.green,
+      color: Colors.black,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
@@ -93,54 +105,36 @@ class Splash extends StatelessWidget {
                 'assets/images/4.jpg',
                 fit: BoxFit.contain,
               )),
-          SizedBox(height: 50),
-         Container(
-              width: double.infinity,
-              alignment: Alignment.center,
-              child: Image.asset(
-                'assets/images/3.jpg',
-                fit: BoxFit.contain,
-              )),
           SizedBox(height: 30),
           Container(
-            child: Text(
-              'Top News',
-              style: GoogleFonts.blinker (
-                fontSize: 35
-              )
-            ),
-            
+            child: Text('Top News',
+                style: GoogleFonts.blinker(fontSize: 35, color: Colors.white)),
           ),
           SizedBox(height: 30),
           Container(
-            child: Text(
-              'Be updated about the latest \n happening around the world.',
-              style: GoogleFonts.blinker (
-                fontSize: 25,
-                fontWeight: FontWeight.w200,
-                wordSpacing: 1.5
-              )
-            ),
-            
+            child: Text('Latest news at your finger tips.',
+                style: GoogleFonts.blinker(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w200,
+                    wordSpacing: 1.5,
+                    color: Colors.white)),
           )
         ],
       ),
     ),
-
   ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
-          
             body: LiquidSwipe(
-      pages: pages,
-      fullTransitionValue: 300,
-      enableSlideIcon: true,
-      enableLoop: true,
-      waveType: WaveType.liquidReveal,
-      positionSlideIcon: 0.5,
-    )));
+          pages: pages,
+          fullTransitionValue: 300,
+          enableSlideIcon: true,
+          enableLoop: true,
+          waveType: WaveType.liquidReveal,
+          positionSlideIcon: 0.5,
+        )));
   }
 }
